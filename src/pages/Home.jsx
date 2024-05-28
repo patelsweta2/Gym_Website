f9906a824237dbf997876b0d678bc181e1784bad
patelsweta2 <sweta0442@gmail.com>
2024-05-28 10:40:24 +0530

    add some images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/hero/Hero";
 import About from "../components/about/About";
+import Featured from "../components/featured/Featured";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <Featured />
     </>
   );
 };
